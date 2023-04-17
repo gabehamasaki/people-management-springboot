83bc3d6a00ee5de3e120766b6d47ade30acd35c5
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2023-04-17 03:34:52 -0300

    first commit

---
+++ src/test/java/com/gabe/peopleManagement/PeopleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabe.peopleManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
